f99861528398572614e18bfb3280843883828c0b
Domagoj <paulic.domagoj@gmail.com>
2024-11-30 10:21:03 +0100

    update main class pascal 2-23 (2), update frontend parser

--- src/main/java/wci/Pascal.java
+++ src/main/java/wci/Pascal.java
@@ -8,8 +8,6 @@ import wci.intermediate.*;
 import wci.backend.*;
 import wci.message.*;
 
-import static wci.message.MessageType.*;
-
 /**
  * <h1>Pascal</h1>
  *
@@ -70,9 +68,9 @@ public class Pascal {
             String flags = "";
 
             // Flags.
-            while ((++i < args.length) && (args[i].charAt(0) == "-")) {
+            while ((++i < args.length) && (args[i].charAt(0) == '-')) {
                 flags += args[i].substring(1);
-            };
+            }
 
             // Source path.
             if(i < args.length) {
@@ -114,7 +112,83 @@ public class Pascal {
     }
 
     private static final String PARSER_SUMMARY_FORMAT =
-            "\n%,20d source lines." +
-            "\n% 20d syntax errors." +
-            "\n%,20.2f seconds total parsing time. \n";
+            """  
+            %,20d source lines.\
+            % 20d syntax errors.\
+            %,20.2f seconds total parsing time.\s
+            """;
+
+    /**
+     * Listener for parser messages
+     */
+    public class ParserMessageListener implements MessageListener {
+        /**
+         * Called by the parser whenever it produces a message
+         * @param message the message that was sent
+         */
+        public void messageReceived(Message message) {
+            MessageType type = message.getType();
+
+            switch (type) {
+                case PARSER_SUMMARY: {
+                    Number[] body = (Number[]) message.getBody();
+                    int statementCount = (Integer) body[0];
+                    int syntaxErrors = (Integer) body[1];
+                    float elapsedTime = (Float) body[2];
+
+                    System.out.printf(PARSER_SUMMARY_FORMAT, statementCount, syntaxErrors, elapsedTime);
+                    break;
+                }
+            }
+        }
+
+    }
+
+    private static final String INTERPRETER_SUMMARY_FORMAT =
+        """
+        %, 20d statements executed.\
+        %, 20d runtime errors.\
+        %, 20.2f seconds total execution time.\s
+        """;
+
+    private static final String COMPILER_SUMMARY_FORMAT =
+            """
+            \n%, 20d instructions generated.
+            \n% 20.2f seconds total code generation time. \n
+            """;
+
+    /**
+     * Listener for back end messages
+     */
+    private class BackendMessageListener implements MessageListener {
+        /**
+         * Called by the back end whenever it produces a message.
+         * @param message the message that was sent
+         */
+        public void messageReceived(Message message) {
+            MessageType type = message.getType();
+
+            switch(type) {
+                case INTERPRETER_SUMMARY: {
+                    Number[] body = (Number[]) message.getBody();
+                    int executionCount = (Integer) body[0];
+                    int runtimeErrors = (Integer) body[0];
+                    float elapsedTime = (Float) body[2];
+
+                    System.out.printf(INTERPRETER_SUMMARY_FORMAT, executionCount, runtimeErrors, elapsedTime);
+                    break;
+                }
+
+                case COMPILER_SUMMARY: {
+                    Number[] body = (Number[]) message.getBody();
+                    int instructionCount = (Integer) body[0];
+                    float elapsedTime = (Float) body[1];
+
+                    System.out.printf(COMPILER_SUMMARY_FORMAT, instructionCount, elapsedTime);
+                    break;
+                }
+            }
+        }
+    }
+
 }
